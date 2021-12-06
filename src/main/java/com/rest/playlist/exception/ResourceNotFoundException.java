10a53bc4c86021b3d1328912013cd4302b9f4f23
kefimo <38332669+kefimo@users.noreply.github.com>
2021-12-06 22:36:10 +0100

    Update ResourceNotFoundException.java

--- src/main/java/com/rest/playlist/exception/ResourceNotFoundException.java
+++ src/main/java/com/rest/playlist/exception/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.rest.playlist.exception;
 
-
+/**
+*   TODO: 
+        -  missing documentation: whith which HTTP code  we can throw it?
+        
+**/
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
