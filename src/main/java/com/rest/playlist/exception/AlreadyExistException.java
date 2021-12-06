c543f1bb3f19fa7eb9f92f4ee16d199bdf39fe0b
kefimo <38332669+kefimo@users.noreply.github.com>
2021-12-06 22:29:06 +0100

    Update AlreadyExistException.java

--- src/main/java/com/rest/playlist/exception/AlreadyExistException.java
+++ src/main/java/com/rest/playlist/exception/AlreadyExistException.java
@@ -1,6 +1,8 @@
 package com.rest.playlist.exception;
 
-
+/**
+*   TODO: missing documentation
+**/
 public class AlreadyExistException extends RuntimeException {
 
     public AlreadyExistException(String message) {
